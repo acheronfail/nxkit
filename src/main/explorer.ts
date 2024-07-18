fae1f0f2a0f2e36998a48b61e84668ceceb817ff
acheronfail <acheronfail@gmail.com>
2024-07-18 14:15:47 +0930

    handle when nand worker unexpectedly dies

--- src/main/explorer.ts
+++ src/main/explorer.ts
@@ -1,16 +1,22 @@
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
-import { BrowserWindow, UtilityProcess, app, utilityProcess } from 'electron';
+import { BrowserWindow, UtilityProcess, app, utilityProcess, dialog } from 'electron';
 import { ExplorerIpcDefinition, ExplorerIpcKey, IncomingMessage, OutgoingMessage } from '../node/nand/explorer.worker';
 
 export class ExplorerWorker {
   private messageId = 0;
+  private exited = false;
   private readonly proc: UtilityProcess;
   private readonly mainWindow: BrowserWindow;
 
   constructor(mainWindow: BrowserWindow) {
     const __dirname = path.dirname(fileURLToPath(import.meta.url));
-    this.proc = utilityProcess.fork(path.join(__dirname, 'src_node_nand_explorer.worker.js'));
+    const workerPath = path.join(__dirname, 'src_node_nand_explorer.worker.js');
+    this.proc = utilityProcess.fork(workerPath, [], { stdio: 'inherit' });
+    this.proc.on('exit', (code) => {
+      console.log(`Worker process exited with code: ${code}`);
+      this.exited = true;
+    });
     this.proc.postMessage({ id: 'bootstrap', isPackaged: app.isPackaged } satisfies IncomingMessage<ExplorerIpcKey>);
     this.mainWindow = mainWindow;
   }
@@ -19,6 +25,15 @@ export class ExplorerWorker {
     channel: C,
     ...args: Parameters<ExplorerIpcDefinition[C]>
   ): ReturnType<ExplorerIpcDefinition[C]> {
+    if (this.exited) {
+      const error = new Error('The NAND worker process has unexpectedly exited');
+      dialog.showMessageBox(this.mainWindow, {
+        type: 'error',
+        message: error.message,
+      });
+      throw error;
+    }
+
     const id = this.messageId++;
     return new Promise((resolve) => {
       const handler = (msg: OutgoingMessage<C>) => {
