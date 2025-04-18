fae135adabf4b37c8fe9b40fce34b2bb88c0ddae
acheronfail <acheronfail@gmail.com>
2025-04-18 16:20:25 +1000

    Revert "try to make output names consistent"
    This reverts commit fae1adb7a56fc7b6b4d35c2c72c7ca2f39776473.

--- forge.config.mjs
+++ forge.config.mjs
@@ -29,22 +29,16 @@ const config = {
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({
-      name: pkgJson.name,
-      setupExe: `${pkgJson.name}-win-setup-\${version}.exe`,
-      setupMsi: `${pkgJson.name}-win-\${version}.msi`,
-    }),
+    new MakerSquirrel({}),
     new MakerZIP({}, ['darwin', 'linux']),
     new MakerRpm({
       options: {
         bin: pkgJson.name,
-        name: 'nxkit-linux-${arch}',
       },
     }),
     new MakerDeb({
       options: {
         bin: pkgJson.name,
-        name: 'nxkit-linux-${arch}',
       },
     }),
   ],
