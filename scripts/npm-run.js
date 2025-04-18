fae13d49ad89394bf98319ca29ba3e95225fa8de
acheronfail <acheronfail@gmail.com>
2025-04-18 13:31:42 +1000

    skip formatting on windows since it's broken

--- scripts/npm-run.js
+++ scripts/npm-run.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 
 import cp from 'node:child_process';
+import os from 'node:os';
 import chalk from 'chalk';
 
-for (const script of process.argv.slice(2)) {
+let windowsAllowFail = false;
+for (const arg of process.argv.slice(2)) {
+  if (arg === '--windows-allow-fail') {
+    if (os.platform() === 'win32') {
+      windowsAllowFail = true;
+    }
+
+    continue;
+  }
+
   try {
-    cp.execSync(`npm run ${script}`, { stdio: 'inherit' });
+    console.log(`${chalk.cyan('running script: ')}${chalk.yellow(arg)}`);
+    cp.execSync(`npm run ${arg}`, { stdio: 'inherit' });
   } catch (err) {
     console.error(chalk.red.bold(err));
-    process.exit(1);
+    process.exit(windowsAllowFail ? 0 : 1);
   }
 }
